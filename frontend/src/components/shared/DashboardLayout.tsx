0d064bb4d7b18e6be1873155389bae6faf40c31e
Muhammad Ayan <f25607218@nutech.edu.pk>
2026-08-31 00:39:19 +0500

    Center dashboard flag watermark

--- frontend/src/components/shared/DashboardLayout.tsx
+++ frontend/src/components/shared/DashboardLayout.tsx
@@ -148,7 +148,7 @@ const DashboardLayout: React.FC<DashboardLayoutProps> = ({ role }) => {
         src="/pakistan-flag-watermark.svg"
         alt=""
         aria-hidden="true"
-        className="pointer-events-none fixed bottom-4 left-3 z-[1] w-64 -rotate-6 select-none opacity-[0.07] blur-[0.2px] sm:w-80 lg:w-[26rem] xl:w-[30rem]"
+        className="pointer-events-none fixed bottom-4 left-10 z-[1] w-64 -rotate-6 select-none opacity-[0.07] blur-[0.2px] sm:left-16 sm:w-80 lg:w-[26rem] xl:w-[30rem]"
       />
 
       <RoleSidebar
